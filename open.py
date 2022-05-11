fe8d5836d0d2035cf32ba390e4cfe729c200d1c8
Guilherme <80716667+Siluryan@users.noreply.github.com>
2022-05-11 09:44:49 -0300

    Update open.py

--- open.py
+++ open.py
@@ -11,3 +11,17 @@ for i in range(3):
 
 
 print(caminho)
+
+
+diretorio = input('Diretório do arquivo: ')
+caminho = open(diretorio,'r+')
+
+
+def escrever(caminho):
+	caminho.write(input('Digite a frase que irá sobrescrever o arquivo: '))
+
+
+escrever(caminho)
+
+
+print(caminho.read())
