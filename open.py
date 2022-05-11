71fedfb9ac4f5a3d9e00152b22fc92911f8cb0d0
Guilherme <80716667+Siluryan@users.noreply.github.com>
2022-05-11 09:18:53 -0300

    Create open.py

---
+++ open.py
@@ -0,0 +1,13 @@
+def abrir(caminho):
+	caminho = open(caminho)
+	print(caminho.read(),'\n')
+
+
+caminho = '/home/guilherme/Which field in the IPv6 header points to optional'
+
+	
+for i in range(3):
+	abrir(caminho)
+
+
+print(caminho)
