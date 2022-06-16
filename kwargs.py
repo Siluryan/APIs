c50ceceb7e248e0bca279fd4d52cca411ea896d4
Guilherme <80716667+Siluryan@users.noreply.github.com>
2022-06-16 16:34:50 -0300

    Create kwargs.py

---
+++ kwargs.py
@@ -0,0 +1,13 @@
+valor = float(input('Valor a ser calculado: \n'))
+
+def kwargs_teste(valor, **kwargs):
+    valor_desconto = kwargs.get('chave_desconto')
+    print(f'valor do desconto: {valor_desconto}')
+
+    if valor_desconto:
+        total = valor - valor_desconto
+
+    return total
+
+kwarg_start = kwargs_teste(valor, chave_desconto = float(input('Valor do desconto: ')))
+print(kwarg_start)
